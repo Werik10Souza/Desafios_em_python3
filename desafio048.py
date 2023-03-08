858339f657f9ac562a2acb4d9aeeb2bfcac4c6e3
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-03-08 17:57:40 -0300

    desafio048

--- desafio048.py
+++ desafio048.py
@@ -1,9 +1,10 @@
 from time import sleep
+soma = 0
 for numero in range(1, 500):
     if numero % 2 == 1:
         print(numero)
         sleep(1)
         multiplo = numero / 3
 print('FIM')
-soma = multiplo + multiplo
+soma += multiplo
 print('\nA soma dos múltiplos de três é igual {:.0f}'.format(soma))
