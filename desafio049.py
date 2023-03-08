f0cf8a291b5a861b503343af88ff59e0981c151e
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-03-08 18:31:25 -0300

    desafio049

--- desafio049.py
+++ desafio049.py
@@ -1,1 +1,13 @@
-#Faça o programa
+for numero in range(1, 10):
+    numero = int(input('Digite um valor: '))
+    print('\n{} X {} = {}'.format(numero, 1, numero*1))
+    print('{} X {} = {}'.format(numero, 2, numero*2))
+    print('{} X {} = {}'.format(numero, 3, numero*3))
+    print('{} X {} = {}'.format(numero, 4, numero*4))
+    print('{} X {} = {}'.format(numero, 5, numero*5))
+    print('{} X {} = {}'.format(numero, 6, numero*6))
+    print('{} X {} = {}'.format(numero, 7, numero*7))
+    print('{} X {} = {}'.format(numero, 8, numero*8))
+    print('{} X {} = {}'.format(numero, 9, numero*9))
+    print('{} X {} = {}'.format(numero, 10, numero*10))
+    
