03ebce653622ea535b08c6d2d7f97aff2384c2aa
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-03-10 15:44:28 -0300

    desafio051

--- desafio051.py
+++ desafio051.py
@@ -1,1 +1,6 @@
-#Faça o DESAFIO
+enesimo_termo = 0
+primeiro_termo = int(input('Digite o primeiro termo da P.A: '))
+razao = int(input('Digite a RAZÃO da P.A: '))
+for primeiro_termo in range(enesimo_termo+1, 9):
+    enesimo_termo = primeiro_termo + (10 - 1) * razao
+    print(enesimo_termo)
