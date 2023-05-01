beccf3207877df6d7ac1e395fc70111eb4786329
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-05-01 20:14:35 -0300

    desafio064

---
+++ desafio064.py
@@ -0,0 +1,3 @@
+
+#* Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a 
+#* condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles(desconsiderando a flag).
