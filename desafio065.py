611878ef61a62c1e57a81a8cef12370b54096333
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-05-01 20:16:14 -0300

    desafio065

---
+++ desafio065.py
@@ -0,0 +1,3 @@
+
+#* Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e 
+#* qual foi o MAIOR e o MENOR valores lindos. O programa deve perguntar do usuário se ele quer ou não continuar a digitar valores.
