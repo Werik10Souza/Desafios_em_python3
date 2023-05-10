d4414bdbe7c4028ea73980ce6d19d469cdbfabb1
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-05-10 15:55:31 -0300

    desafio63

--- desafio063.py
+++ desafio063.py
@@ -1,16 +1,18 @@
 
 #* Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma Sequência de Fibonacci.
+#? t = termo
 #* EX:
 #* 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+#  t1 + t2 = t3
 
 numero = int(input('Quantos elementos você quer ver?: '))
-primeiro = 0
-segundo = 1 
+t1 = 0
+t2 = 1 
 contador = 3
-print(primeiro, segundo, end= ' ')
+print(t1, t2, end= ' ')
 while contador <= numero:
-    terceiro = primeiro + segundo
-    print(terceiro, end= ' ')
-    primeiro = segundo
-    segundo = terceiro
+    t3 = t1 + t2
+    print(t3, end= ' ')
+    t1 = t2
+    t2 = t3
     contador = contador + 1
