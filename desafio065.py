052e5ac9cbc68c5377dd88cdb651b811fb094f4e
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-05-15 11:57:51 -0300

    desafio065

--- desafio065.py
+++ desafio065.py
@@ -2,28 +2,28 @@
 #* Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e 
 #* qual foi o MAIOR e o MENOR valores lindos. O programa deve perguntar do usuário se ele quer ou não continuar a digitar valores.
 
-contador = 0
-media = 0
+pergunta = 'SIM'
 soma = 0
+quantidade = 0
 maior = 0
 menor = 0
-pergunta = 'sim'
-
-while pergunta == 'sim':
-    numero = int(input('\nDigite um número inteiro da sua escolhar: '))
-    pergunta = str(input('\nDigite [sim/nao], para continuar: ')).strip().upper()
-    while pergunta != 'sim' and pergunta != 'nao':
-        pergunta = str(input('\nDigite [sim/nao], para continuar: ')).strip()
+while pergunta in 'SIMsim': 
+    numero = int(input('Digite um valor: '))
+    pergunta = str(input('Quer continuar [SIM/NAO]: ')).upper()
+    quantidade = quantidade + 1
     soma = soma + numero
-    contador = contador + 1
-    media = soma / contador
-    if contador == 1:
-        maior = menor = numero
+    if quantidade == 1:
+        maior = menor
+        menor = numero
     else:
         if numero > maior:
             maior = numero
-        elif numero < menor:
+        if numero < menor:
             menor = numero
-print('\nMEDIA: {:.2f}'.format(media))
+media = soma / quantidade
+
+print('\nSOMA: {}'.format(soma))
+print('Quantidade: {}'.format(quantidade))
 print('MAIOR: {}'.format(maior))
 print('MENOR: {}'.format(menor))
+print('MÉDIA: {}'.format(media))
