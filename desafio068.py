40b1e570fcd38be5111d95d33563ac40d53ac1ec
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-05-19 18:46:55 -0300

    desafio068

---
+++ desafio068.py
@@ -0,0 +1,2 @@
+
+#*Faça um programa que jogue PAR ou IMPAR com o computador. Jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivads que ele conquistou no final do jogo.
