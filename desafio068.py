736d2b4e0753e00f7504f1a92ffc213f83afc73c
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-06-22 14:37:43 -0300

    desafio068

--- desafio068.py
+++ desafio068.py
@@ -1,14 +1,14 @@
 
 #*Faça um programa que jogue PAR ou IMPAR com o computador. Jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 
+#?OPÇÃO 01:
+
 import random
 
 lista = ['par', 'impar']
 
 computador = (random.choice(lista))
 
-print(computador)
-print('\n')
 
 contador = 0
 
@@ -27,3 +27,38 @@ while (computador != lista):
         print('VOCÊ PERDEU! O computador escolheu {}'.format(computador))
         break
 print('\nTOTAL DE VITÓRIAS CONSECUTIVAS: {}'.format(contador))
+
+#!-----------------------------------//--------------------------------//-----------------------
+
+#?OPÇÃO 02:
+
+from random import randint
+
+contador = 0
+
+while True:
+    jogador = int(input("Digite 0[PAR] e 1[ÍMPAR]: "))
+
+    sorteo = randint(0, 1)
+
+    ganhou = jogador == sorteo
+
+    if ganhou:
+        print("GANHOU!")
+    else:
+        print("PERDEU! Era {}".format(sorteo))
+        break
+
+    contador = contador + 1
+
+print("\n")
+print("-------//-----")
+print("0 -> PAR")
+print("1 -> ÍMPAR")
+print("-------//-----")
+print("\n")
+print("========//=========")
+print("FIM DE JOGO!")
+print("========//=========")
+print("\n")
+print("VITÓRIAS CONSECUTIVAS: {}".format(contador))
