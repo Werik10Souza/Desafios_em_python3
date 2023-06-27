432e0b234cc15b3abd44f8f542d5a35fd51a640e
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-06-27 17:18:31 -0300

    update

--- desafio071.py
+++ desafio071.py
@@ -3,23 +3,26 @@
 #*A SER SACADO(número inteiro) e o programa vai informar quantas CÉLULAS de cada valor serão entregues.
 #? OBS: Considere que o caixa possui células de R$50, R$20, R$10 e R$1
 
-valor_sacado = 0
-
+print("="*45)
+print("{:^45}".format('CAIXA ELETRÔNICO'))
+print("="*45)
+valor = int(input("Digite o valor quer queira sacar: "))
+cedula = 50
+total_cedula = 0
 while True:
-    print('='*50)
-    print('CAIXA ELETRÔNICO')
-    print('='*50)
-    print('Digite o valor desejado AQUI EMBAIXO!!!!!')
-    valor_sacado = int(input('\nDIGITE: '))
-    cinquenta = valor_sacado/30
-    vinte = cinquenta / 10
-    dez = vinte/ 9
-    um = dez / 1
-    break
-print('\n')
-print('='*50)
-print('QUANTIDADE DE R$50: {:.0f}'.format(cinquenta))
-print('QUANTIDADE DE R$20: {:.0f}'.format(vinte))
-print('QUANTIDADE DE R$10: {:.0f}'.format(dez))
-print('QUANTIDADE DE R$1: {:.0f}'.format(um))
-print('='*50)
+    if valor >= cedula:
+        valor = valor - cedula
+        total_cedula = total_cedula + 1
+    else:
+        if total_cedula > 0:
+            print('\nA quantidade {} cédula(s) de R${}'.format(total_cedula, cedula))
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        total_cedula = 0
+        if valor == 0:
+            break
+print('\nFIM!!!')
