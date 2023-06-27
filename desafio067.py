6ad4f715e15fbf33bce451429afcbfa521ae72b3
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-06-26 22:00:33 -0300

    update da tabuada

--- desafio067.py
+++ desafio067.py
@@ -4,18 +4,10 @@
 
 while True:
     numero = int(input('Digite um valor: '))
-    if numero <= -1:
+    if numero <= 0:
         break
-    print('\n{} X {} = {}'.format(numero, 1, numero*1))
-    print('{} X {} = {}'.format(numero, 2, numero*2))
-    print('{} X {} = {}'.format(numero, 3, numero*3))
-    print('{} X {} = {}'.format(numero, 4, numero*4))
-    print('{} X {} = {}'.format(numero, 5, numero*5))
-    print('{} X {} = {}'.format(numero, 6, numero*6))
-    print('{} X {} = {}'.format(numero, 7, numero*7))
-    print('{} X {} = {}'.format(numero, 8, numero*8))
-    print('{} X {} = {}'.format(numero, 9, numero*9))
-    print('{} X {} = {}'.format(numero, 10, numero*10))
+    for contador in range(1, 11):
+        print('{} X {} = {}'.format(numero, contador, numero*contador))
 print('\n')
 print('-'*40)
 print('FIM DO PROGRAMA!')
