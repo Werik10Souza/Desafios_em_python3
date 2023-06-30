1d31a3c7c59b22a02fcf6b36a5ccfcb1f65805ba
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-06-30 12:10:58 -0300

    update

--- desafio073.py
+++ desafio073.py
@@ -4,4 +4,23 @@
 #?A) Apenas os 5 primeiros colocados.
 #?B) Os últimos 4 colocados da tabela.
 #?C) Uma lista com os times em ordem alfabética.
-#?D) Em que posição na tabela está o time da Chapecoense.
+#?D) Em que posição na tabela está o time da Flamengo.
+
+
+tabela = ('', 'Botafogo', 'Grêmio', 'Flamengo', 'Palmeiras', 'Fluminese', 'Internacional', 'Bragantino', 'Fortaleza', 'Athletico-PR', 'Atlético-MG', 'São Paulo', 'Cruzeiro', 'Santos', 'Bahia', 'Corinthians', 'Cuiabá', 'Goiás', 'Vasco da Gama', 'América-MG', 'Coritiba')
+
+print('='*25)
+print('{}'.format('Classificação Série A'))
+print('='*25)
+
+print('\nOs cinco primeiros colocados de 2023:\n')
+print(tabela[1:6])
+
+print('\nOs últimos quarto colocados de 2023:\n')
+print(tabela[-4:])
+
+print('\nOrdem Alfabéica:\n')
+print(sorted(tabela))
+
+print('Posição do Flamengo na tabela do campeonato brasileiro de 2023:\n')
+print(tabela.index('Flamengo'))
