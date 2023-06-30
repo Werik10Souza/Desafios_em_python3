1dfac1549c74ba246267da413761f95f76f95815
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-06-30 15:45:23 -0300

    update

--- desafio075.py
+++ desafio075.py
@@ -6,3 +6,29 @@
 #? B) Em que posição foi digitado o primeiro valor 3?.
 
 #? C) Quais foram os números 'pares'.
+
+lista = ()
+
+for lista01 in range(1):
+    valor = int(input('Digite um valor inteiro qualquer: '))
+    lista01 = valor
+    for lista02 in range(1):
+        valor = int(input('Digite um valor inteiro qualquer: '))
+        lista02 = valor
+    for lista03 in range(1):
+        valor = int(input('Digite um valor inteiro qualquer: '))
+        lista03 = valor
+        for lista04 in range(1):
+            valor = int(input('Digite um valor inteiro qualquer: '))
+            lista04 = valor
+    lista = lista01, lista02, lista03, lista04
+print('\n', lista, '\n')
+
+print('='*20)
+print('parece o valor nove:')
+print('\n', lista.count(9), 'vezes')
+print('='*20)
+print('\n', '='*40)
+print('O valor três foi digitado na posição:')
+print(lista.index(3))
+print('='*40)
