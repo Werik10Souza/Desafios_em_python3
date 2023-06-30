0cd8eca2c833d5a0a86be431979f6fcc491b1e3f
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-06-30 11:02:31 -0300

    update

--- desafio072.py
+++ desafio072.py
@@ -2,3 +2,9 @@
 #*Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extensão, de zero até vinte.
 #*Seu programa deverá ler um número pelo teclado(entre 0 e 20) e mostrá-lo por extenso.
 
+contagem_extensao = ('Zero', 'Um', 'Dois', 'Três', 'Quarto', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Desezoito', 'Dezenove', 'Vinte')
+
+teclado  = int(input('\nDigite um numero entre 0 e 20: '))
+
+print('\n')
+print(contagem_extensao[teclado])
