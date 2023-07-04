5ea1c627c03372d9873135f2a062658ad21736c6
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-04 14:40:03 -0300

    update

--- desafio076.py
+++ desafio076.py
@@ -7,13 +7,13 @@ print('='*60)
 print('            LISTA DE COMPRAS')
 print('='*60)
 
-lista_de_compras = ('\nQueijo ........................R$7.20 (1 unidade)'
-                    '\nBatata Doce ....................R$5.00 (1KG)'
-                    '\nArroz ..........................R$6.25 (1KG)'
-                    '\nFeijão .........................R$4.78 (1KG)'
-                    '\nCarne ...........................R$50.00 (1KG)'
-                    '\nFarinha ..........................R$2.82 (1KG)'
-                    '\nManteiga .........................R$4.58 (1unidade)'
-                    '\nLata de sardinha ..................R$3.12 (1unidade)\n')
+lista_de_compras = ('\nQueijo ........................R$ 7.20 (1 unidade)'
+                    '\nBatata Doce ....................R$ 5.00 (1KG)'
+                    '\nArroz ..........................R$ 6.25 (1KG)'
+                    '\nFeijão .........................R$ 4.78 (1KG)'
+                    '\nCarne ...........................R$ 50.00 (1KG)'
+                    '\nFarinha ..........................R$ 2.82 (1KG)'
+                    '\nManteiga .........................R$ 4.58 (1unidade)'
+                    '\nLata de sardinha ..................R$ 3.12 (1unidade)\n')
 print(lista_de_compras)
 print('='*60)
