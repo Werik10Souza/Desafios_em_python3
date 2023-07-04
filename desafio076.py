d894073a6b45d18e3102620bc4638398145e8f19
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-04 14:18:54 -0300

    update

--- desafio076.py
+++ desafio076.py
@@ -2,3 +2,13 @@
 #*Crie um programa que tenha uma tupla única com 'nomes de produtos' e seus respectivos preços, na sequência.
 
 #*No final, mostre uma listagem de preços, organizando os dados em forma 'tabular'.
+
+lista_de_compras = ('\nQueijo ........................R$7.20 (1 unidade)'
+                    '\nBatata Doce ....................R$5.00 (1KG)'
+                    '\nArroz ..........................R$6.25 (1KG)'
+                    '\nFeijão .........................R$4.78 (1KG)'
+                    '\nCarne ...........................R$50.00 (1KG)'
+                    '\nFarinha ..........................R$2.82 (1KG)'
+                    '\nManteiga .........................R$4.58 (1unidade)'
+                    '\nLata de sadinha ..................R$3.12 (1unidade)\n')
+print(lista_de_compras)
