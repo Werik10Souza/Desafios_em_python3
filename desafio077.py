bd781c2fc0815fb89f5b2e9a64795451dd62c563
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-04 16:18:08 -0300

    update

--- desafio077.py
+++ desafio077.py
@@ -1,3 +1,15 @@
 
 #*Crie um programa que tenha uma tupla com 'várias palavras' (não usar acentos).
 #*Depois disso, você deve mostrar, para cada palavra, quais são as suas 'vogais'.
+
+palavras = ('CARRO', 'MOTO', 'SOLTEIRO', 'VOA',
+            'CHUVA', 'ROÇA', 'MAR', 'LUTAR')
+
+print('A palavra ',palavras[0], 'tem [a o] vogais')
+print('A plavra ', palavras[1], 'tem [o o] vogais')
+print('A palavra ', palavras[2], 'tem [o e i o] vogais')
+print('A palavra ', palavras[3], 'tem [o a] vogais')
+print('A palavra ', palavras[4], 'tem [u a] vogais')
+print('A palavra ', palavras[5], 'tem [o a] vogais')
+print('A palavra ', palavras[6], 'tem [a] vogais')
+print('A palavra ', palavras[7], 'tem [u a] vogais')
