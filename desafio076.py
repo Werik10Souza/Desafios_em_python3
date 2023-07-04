8c82bad204c3cfcbc01666fe2de9efa416c05001
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-04 14:37:08 -0300

    update

--- desafio076.py
+++ desafio076.py
@@ -3,6 +3,10 @@
 
 #*No final, mostre uma listagem de preços, organizando os dados em forma 'tabular'.
 
+print('='*60)
+print('            LISTA DE COMPRAS')
+print('='*60)
+
 lista_de_compras = ('\nQueijo ........................R$7.20 (1 unidade)'
                     '\nBatata Doce ....................R$5.00 (1KG)'
                     '\nArroz ..........................R$6.25 (1KG)'
@@ -12,3 +16,4 @@ lista_de_compras = ('\nQueijo ........................R$7.20 (1 unidade)'
                     '\nManteiga .........................R$4.58 (1unidade)'
                     '\nLata de sardinha ..................R$3.12 (1unidade)\n')
 print(lista_de_compras)
+print('='*60)
