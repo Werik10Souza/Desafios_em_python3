8035dfbe2e0aa0724eb68fa91fb024753907a558
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-07 13:51:34 -0300

    update

--- desafio078.py
+++ desafio078.py
@@ -1,3 +1,13 @@
 
 #*Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
-#*No final, mostre qual foi o e o MENOR valor digitado e as suas respectivas posições na lista.
+#*No final, mostre qual foi o MAIOR e o MENOR valor digitado e as suas respectivas posições na lista.
+
+numeros = []
+
+for contador in range(0,5):
+    numeros.append(int(input('Digite o valor: ')))
+print('\n',numeros)
+print('\nFoi digitado na posição {}, o número'.format(numeros.index(max(numeros))), end='')
+print(' {}, o MAIOR valor inserido'.format(max(numeros)))
+print('foi digitado na posição {}, o número'.format(numeros.index(min(numeros))), end='')
+print(' {}, o MENOR valor inserido'.format(min(numeros)))
