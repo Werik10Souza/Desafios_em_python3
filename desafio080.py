bfc2abf1b17222cb39bb2df87401546ab4434fce
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-10 13:52:58 -0300

    update

--- desafio080.py
+++ desafio080.py
@@ -1,3 +1,20 @@
 
 #*Crie um programa onde o usuário possa digitar cinco VALORES NUMÉRICOS e cadastre-os em uma lista. JÁ , mostre a  POSIÇÃO CORRETA de inserção(sem usar o sort()).
 #*No final, mostre a LISTA ORDENADA na tela.
+
+valores = []
+
+for contador in range(0,5):
+    numero = int(input('Digite o valor: '))
+    if contador == 0: #O número zero(0), significa o ÍNDICE/POSIÇÃO
+        valores.append(numero)
+    elif numero > valores[-1]:
+        valores.append(numero)
+    else:
+        posicao = 0
+        while posicao < len(valores):
+            if numero <= valores[posicao]:
+                valores.insert(posicao, numero)
+                break
+            posicao = posicao + 1
+print('\nOs valores digitados em ordem foram {}'.format(valores))
