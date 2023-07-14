4ba97ef527116b53b33b23935b7f12c9c995241a
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-14 16:06:14 -0300

    update

--- desafio083.py
+++ desafio083.py
@@ -1,3 +1,21 @@
 
 #*Crie um programa onde o usuário digite uma EXPRESSÃO qualquer que usa PARÊNTESES.Seu aplicativo deverá analisar se
 #*a expressão passada está com os parênteses abertos e fechados na ORDEM CORRETA.
+
+expressao = str(input('Digite a expressão: '))
+
+pilha = []
+
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
