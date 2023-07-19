677883a3f70373d29e1e80547d8bf0a96efe35ba
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-19 17:38:39 -0300

    update

--- desafio084.py
+++ desafio084.py
@@ -8,3 +8,34 @@
 #?C)Uma listagem com as pessoas mais LEVES.
 
 
+pessoas = []
+dados = []
+
+resposta = 'N'
+
+contador = 0
+
+pesada = 0
+leve = 0
+
+while True:
+    nome = str(input('\nDigite o seu nome: '))
+    peso = float(input('Digite o seu peso: '))
+    contador = contador + 1
+    resposta = str(input('\nQuer continuar[S/N]?: ').upper().strip() [0])
+    if nome not in dados:
+        dados.append(nome)
+    if peso not in dados:
+        dados.append(peso)
+    pessoas.append(dados[:])
+    dados.clear()
+    if resposta == 'N':
+        break
+print('\n',pessoas)
+print('\nPessoas cadastradas: {}'.format(contador))
+for pessoa in pessoas:
+    if pessoa[1] >= 100:
+        print('ºpesado: {}'.format(pessoa[0]))
+    else:
+        if pessoa[1] <= 65:
+            print('ºleve: {}'.format(pessoa[0]))
