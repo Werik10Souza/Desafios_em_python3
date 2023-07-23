b353c0a7664e1909b45d23d7b13ddce6808996dc
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-23 15:28:15 -0300

    update

--- desafio086.py
+++ desafio086.py
@@ -6,3 +6,25 @@
 #! |_|_|_|
 
 #?No final, mostre a MATRIZ na tela, com a formatação correta
+
+matriz = []
+numeros = []
+
+for valor in range(0,9):
+    valor = int(input('Digite um número inteiro: '))
+    if valor not in numeros:
+        numeros.append(valor)
+    matriz.append(numeros[:])
+    numeros.clear()
+
+print('\n')
+
+print(sorted(matriz))
+print('\n')
+
+print('M A T R I Z')
+print('='*20)
+print(matriz[0], matriz[1], matriz[2])
+print(matriz[3], matriz[4], matriz[5])
+print(matriz[6], matriz[7], matriz[8])
+print('='*20)
