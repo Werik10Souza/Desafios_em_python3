329f5d09340472112e0da78c2efb184d829c2a16
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-23 00:49:07 -0300

    update

--- desafio085.py
+++ desafio085.py
@@ -1,23 +1,18 @@
 
 #*Crie um programa onde o usuário possa digitar SETE VALORES NUMÉRICOS e cadastre-os em uma LISTA ÚNICA que mantenha separados os valores PARES e IMPARES. No final, mostre os valores PARES e IMPARES em ordem CRESCENTE.
 
-valores = []
-par = []
-impar = []
+valores = [[], []]
 
 for valor in range(0,7):
-    numero = int(input('Digite um valor: '))
+    numero = int(input('Digite o valor: '))
     if numero not in valores:
-        valores.append(numero)
-    if numero not in par:
         if numero % 2 == 0:
-            par.append(numero)
-    if numero not in impar:
-        if numero % 2 != 0:
-            impar.append(numero)
-    valores.append(par[:])
-    valores.append(impar[:])
+            valores[0].append(numero)
+        elif numero % 2 != 0:
+            valores[1].append(numero)
+
+print('\n',valores)
 
 print('\n','='*20)
-print('PAR: ',sorted(par))
-print('IMPAR: ',sorted(impar))
+print('PAR: ',sorted(valores[0]))
+print('IMPAR: ',sorted(valores[1]))
