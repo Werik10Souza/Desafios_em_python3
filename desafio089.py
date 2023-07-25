cca17bdff91118c7173d053da41099b1fde95129
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-25 18:06:10 -0300

    update

--- desafio089.py
+++ desafio089.py
@@ -25,23 +25,3 @@ while True:
 
 print('\n', pessoas, '\n')
  
-print('='*50)
-print('LISTA DE NOMES E NOTAS DE ALUNOS')
-print('='*50)
-print('\nNome: {} Média: {}'.format(pessoas[0][0], (pessoas[0][1]+pessoas[0][2])/2))
-print('\nNome: {} Média: {}'.format(pessoas[1][0], (pessoas[1][1]+pessoas[1][2])/2))
-print('\nNome: {} Média: {}'.format(pessoas[2][0], (pessoas[2][1]+pessoas[2][2])/2))
-
-pergunta = str(input('\nQuer pesquisa um aluno expressifico?: ').upper().strip() [0])
-if pergunta != 'N':
-    print('-'*30)
-    print('BUSCAR POR ALUNO EXPERSSIFICO...')
-    print('-'*30)
-    buscar = str(input('Nome do aluno: '))
-
-    #?ANALISAR O CÓDIGO E RESOLVER
-     
-    if buscar == pessoas[0][0]:
-        print(pessoas[0][0])
-    else:
-        print('ERRO!!!')
