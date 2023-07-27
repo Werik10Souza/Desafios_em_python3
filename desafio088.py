96120d9f344c99a9da9f702914610bd4dba19ea5
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-07-27 17:58:08 -0300

    update

--- desafio088.py
+++ desafio088.py
@@ -1,22 +1,2 @@
 
 #*Faça um programa que ajude um jogador da MEGA SENA a criar PALPITES. O programa vai perguntar QUANTOS JOGOS serão gerados e vai sortear 6 NÚMEROS ENTRE 1 A 60 para cada jogo, cadastrando tudo em uma LISTA COMPOSTA.
-
-from random import randint
-
-palpites = []
-numeros = []
-
-jogos = int(input('Quantos jogos serão gerados?: '))
-print('\n')
-
-for numero in range(0,6):
-    numero = randint(1,60)
-    if numero not in numeros:
-        numeros.append(numero)
-        palpites.append(numeros[:])
-        numeros.clear()
-print('='*60)
-print('                     P A L P I T E S')
-print('='*60)
-
-print(palpites * jogos)
