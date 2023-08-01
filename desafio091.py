a58bf9f017606ab48e1f1ddb269869471e1808a2
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-01 17:42:55 -0300

    update

---
+++ desafio091.py
@@ -0,0 +1,5 @@
+
+#*Crie um programa onde 4 JOGADORES joguem um DADO e tinham resultados ALEATÓRIOS. Grande esses resultados em um
+#*DICIONÁRIO. No final, coloque esse dicionário em ORDEM, sabendo que o VENCEDOR tirou o MAIOR NÚMERO no dado.
+
+
