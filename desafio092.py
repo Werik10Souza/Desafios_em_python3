1153fdd2f16dead83212553f65883a2688e23282
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-02 22:00:50 -0300

    update

--- desafio092.py
+++ desafio092.py
@@ -3,3 +3,9 @@
 #*se por acaso o CTPS for diferente de ZERO, o dicionário receberá também o ANO DE CONTRATAÇÃO. Calcule e acrescente,
 #*além da IDADE, com quantos anos a pessoa vai se aposentar?
 
+nome = str(input('Nome: '))
+ano_de_nascimento = int(input('Ano de Nascimento: '))
+
+carreira = {'Nome': nome, 'Idade' : 2023 - ano_de_nascimento}
+
+print(carreira)
