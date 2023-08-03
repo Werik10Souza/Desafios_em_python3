92c5da555fd2039b63a1ce098f0badad28126afc
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-02 21:37:17 -0300

    update

---
+++ desafio092.py
@@ -0,0 +1,5 @@
+
+#*Crie um programa que LEIA NOME,ANO DE NASCIMENTO e CANTEIRA DE TRABALHO e cadastre-os (com IDADE) com um dicionário
+#*se por acaso o CTPS for diferente de ZERO, o dicionário receberá também o ANO DE CONTRATAÇÃO. Calcule e acrescente,
+#*além da IDADE, com quantos anos a pessoa vai se aposentar?
+
