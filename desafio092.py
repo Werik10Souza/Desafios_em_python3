485f1456f1430822478793dcb8d0ea182397f968
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-03 21:40:57 -0300

    update

--- desafio092.py
+++ desafio092.py
@@ -3,9 +3,26 @@
 #*se por acaso o CTPS for diferente de ZERO, o dicionário receberá também o ANO DE CONTRATAÇÃO. Calcule e acrescente,
 #*além da IDADE, com quantos anos a pessoa vai se aposentar?
 
+from datetime import datetime
+
 nome = str(input('Nome: '))
 ano_de_nascimento = int(input('Ano de Nascimento: '))
+ano_atual = datetime.now()
+ano = ano_atual.year
 
-carreira = {'Nome': nome, 'Idade' : 2023 - ano_de_nascimento}
+carteira_trabalho = int(input('Carteira de Trabalho(0 não tenho): '))
 
-print(carreira)
+idade = ano - ano_de_nascimento
+
+if carteira_trabalho != 0:
+    ano_contratacao = int(input('Ano de contratação: '))
+    salario = int(input('Salário:R$'))
+    carreira = {'Nome': nome, 
+                'Idade' :  idade,
+                'CPTS' : carteira_trabalho,
+                'Contratação' : ano_contratacao,
+                'Salário' : salario,
+                'Aposentadoria' : 35 - (ano_de_nascimento - ano_contratacao)}
+
+    print('='*110)
+    print(carreira)
