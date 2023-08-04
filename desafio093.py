5d851e61e40197a9af6042412eefe8c01d55150d
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-04 19:34:02 -0300

    update

---
+++ desafio093.py
@@ -0,0 +1,6 @@
+
+#*Crie um programa que gerencie o aproveitamento de um JOGADOR DE FUTEBOOL. O programa vai ler o NOME DO JOGADOR e 
+#*QUANTAS PARTIDAS ele jogou. Depois vai ler a QUANTIDADE DE GOLS feitos em CADA PARTIDA. No final, tudo isso será
+#*guardado em um DICIONÁRIO, incluindo o TOTAL DE GOLS feitos durante o campeonato.
+
+
