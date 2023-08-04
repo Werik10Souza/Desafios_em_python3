5c424d8aaf7be6e8d303f23bc629913b0b4f50d9
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-04 19:48:52 -0300

    update

--- desafio093.py
+++ desafio093.py
@@ -3,4 +3,20 @@
 #*QUANTAS PARTIDAS ele jogou. Depois vai ler a QUANTIDADE DE GOLS feitos em CADA PARTIDA. No final, tudo isso será
 #*guardado em um DICIONÁRIO, incluindo o TOTAL DE GOLS feitos durante o campeonato.
 
+nome = str(input('Nome do Jogador: '))
+quantidades_partidas = int(input(f'Quantas partidas {nome} jogou?: '))
 
+contador = 0
+
+lista_gols = []
+
+while contador < quantidades_partidas:
+    gols_partida = int(input('Quantos gols na partida?: '))
+    lista_gols.append(gols_partida)
+    contador = contador + 1
+
+print('\n',lista_gols)
+
+#analise = {'Nome' : nome,
+           #'Gols' : lista_gols,
+           #'Toal' : total}
