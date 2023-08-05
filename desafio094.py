a2a795963261b4745e3a45f4be0aeff629256b78
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-05 15:01:59 -0300

    update

--- desafio094.py
+++ desafio094.py
@@ -11,6 +11,10 @@ soma_idades = 0
 
 lista = []
 
+mulheres_lista = []
+
+acima_media = []
+
 while True:
     nome = str(input('\nNome: '))
     sexo = str(input('Sexo [M/F]: ').upper().strip() [0])
@@ -23,6 +27,13 @@ while True:
              'Idade' : idade}
     
     lista.append(dados.copy())
+    for dados in lista:
+        if idade > (soma_idades / contador):
+            acima_media.append(dados['Nome'])
+
+    if sexo == 'F':
+        mulheres_lista.append(nome)
+    
 
     resposta = str(input('\nQuer continuar [S/N]: ').upper().strip() [0])
 
@@ -31,7 +42,11 @@ while True:
 
 media = soma_idades / contador
 
+
+print('\n')
+print(lista)
 print('-='*40)
 print(f'O grupo tem {contador} pessoas.')
 print(f'A média de idade é de {media} anos.')
-print(lista)
+print(f'As mulheres cadastradas foram: {mulheres_lista}')
+print(f'Lista das pessoas que estão acima da média: {acima_media}')
