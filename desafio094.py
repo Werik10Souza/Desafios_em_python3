e41f4ab1fe8c27f853e183139d18854f44064182
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-04 21:36:06 -0300

    update

---
+++ desafio094.py
@@ -0,0 +1,9 @@
+
+#*Crie um programa que leia NOME, SEXO e IDADE de VÁRIOS PESSOAS, guardando os dados de cada pessoa em um DICIONÁRIO
+#*e todos os dicionários em uma LISTA. No final, mostre:
+#?A) Quantas pessoas foram cadastradas.
+#?B) A média de idade do grupo.
+#?C) Uma lista com todas as mulheres.
+#?D) Uma lista com todas as pessoas com idade acima da média.
+
+
