9f8a2229b04c79720d5a5f94c1fbbb2e1948d928
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-18 18:03:26 -0300

    update

--- desafio095.py
+++ desafio095.py
@@ -2,35 +2,4 @@
 #*Aprimore o DESAFIO 093 para que ele funcione com VÁRIOS JOGADORES, incluindo um sistema de visualização de DETALHES
 #*DO APROVETAMENTO de cada jogador.
 
-while True:
-    nome = str(input('Nome do Jogador: '))
-    quantidades_partidas = int(input(f'Quantas partidas {nome} jogou?: '))
 
-    contador = 0
-    total = 0
-
-    lista_gols = []
-
-    while contador < quantidades_partidas:
-        gols_partida = int(input(f'Quantos gols na partida {contador}?: '))
-        lista_gols.append(gols_partida)
-        contador = contador + 1
-        total = total + gols_partida
-
-    analise = {'Nome' : nome,
-            'Gols' : lista_gols,
-            'Total' : total}
-    
-    while True:
-        resp = str(input('Quer continuar?[S/N]: ')).upper() [0]
-        if resp in 'SN':
-            break
-        print('Erro! Responda apenas S ou N.')
-    if resp == 'N':
-        break
-
-print('Codigo', end=' ')
-for indice in analise.keys():
-    print(f'{indice:<15}', end='')
-print()
-print('-'*40)
