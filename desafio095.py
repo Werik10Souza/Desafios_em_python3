f82d2d04621b176f4861defda5c3bd9e3d178cf3
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-18 18:38:05 -0300

    update

--- desafio095.py
+++ desafio095.py
@@ -2,4 +2,48 @@
 #*Aprimore o DESAFIO 093 para que ele funcione com VÁRIOS JOGADORES, incluindo um sistema de visualização de DETALHES
 #*DO APROVETAMENTO de cada jogador.
 
+time = []
+jogador = {}
+partidas = []
 
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do Jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'Quantos gols na partida {c}? ')))
+    jogador["gols"] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resposta = str(input('Quer continuar[S/N]?').upper() [0])
+        if resposta in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resposta == 'N':
+        break
+print('-='*30)
+print('cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-'*40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-'*40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com código {busca}!')
+    else:
+        print(f' --LEVATAMENTO DO JOGADOR {time[busca] ["nome"]}:')
+        for i, g in enumerate(time[busca] ['gols']):
+            print(f'No jogo {i+1} fez {g} gols.')
+    print('-'*40)
+print('<< VOLTE SEMPRE >>')
