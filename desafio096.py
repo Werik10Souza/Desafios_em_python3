809498f16a93758f80675ef9c5baec4219f8cff1
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-20 17:30:48 -0300

    update

--- desafio096.py
+++ desafio096.py
@@ -1,3 +1,15 @@
 
 #*Faça um programa que tenha uma FUNÇÃO chamada AREA(), que receba as dimensões de um terreno retangular(largura e comprimento) e mostre a ÁREA DO TERRENO.
 
+import math
+
+def area(largura, comprimento):
+    print('-'*30)
+    largura = float(input('LARGURA(m): '))
+    comprimento = float(input('COMPRIMENTO(m): '))
+    tamanho = largura * comprimento
+    print(f'A área de um terreno {largura}X{comprimento} é de {tamanho} metros quadrados.')
+    print('\n')
+
+area('largura', 'comprimento')
+
