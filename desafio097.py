bd124a219355f7c09eb9afe69a78b6ba3e8af122
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-20 17:44:51 -0300

    update

---
+++ desafio097.py
@@ -0,0 +1,9 @@
+
+#*Faça um programa que tenha uma FUNÇÃO chamada ESCREVA(), que receba um texto qualquer com PARÂMETRO e mostre uma mensagem com tamanho adaptável.
+    #?EX:
+        #*escreva('Olá, mundo!')
+    #?SAÍDA:
+     #*~~~~~~~~~~
+     #*Olá, mundo!
+     #*~~~~~~~~~~
+
