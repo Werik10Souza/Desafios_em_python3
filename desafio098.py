f7612fda58386fe2fdb09b2704aeb37a2889e92e
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-21 12:20:51 -0300

    update

---
+++ desafio098.py
@@ -0,0 +1,9 @@
+
+#*Faça um programa que tenha uma FUNÇÃO chamada CONTADOR(), que receba três PARÂMETROS: INICIO, FIM e PASSO e realizar a contagem.
+
+#*Seu programa tem que realizar TRÊS CONTAGENS através da função criada:
+
+#?a) De 1 até 10, de 1 em 1
+#?b) De 10 até 0, de 2 em 2
+#?c) Uma contagem PERSONALIZADA.
+
