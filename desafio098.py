e4fcbb5a97a78144209bb587059f28de21d922a4
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-21 14:05:57 -0300

    update

--- desafio098.py
+++ desafio098.py
@@ -7,3 +7,19 @@
 #?b) De 10 até 0, de 2 em 2
 #?c) Uma contagem PERSONALIZADA.
 
+def letra_a():
+    print('-='*30)
+    print('Contagem de 1 até 10, de 1 em 1')
+    for x in range(1,11,1):
+        print(x, end=' ')
+    print('FIM!')
+
+letra_a()
+
+
+#def contador(inicio, fim, passo):
+    #inicio = int(input('Inicio: '))
+    #fim = int(input('Fim: '))
+    #passo = int(input('Passo: '))
+
+#contador('inicio', 'fim', 'passo')
