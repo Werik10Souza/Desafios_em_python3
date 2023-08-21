f731b91f57fa0e2ae146483334fd4937ba286210
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-21 15:41:00 -0300

    update

---
+++ desafio099.py
@@ -0,0 +1,4 @@
+
+#*Faça um programa que tenha uma FUNÇÃO chamada MAIOR(), que receba vários PARÂMETROS com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles e o MAIOR.
+
+
