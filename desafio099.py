c3337f5eb2bc595acb4662a842c4bff6bd46a284
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-21 16:32:46 -0300

    update

--- desafio099.py
+++ desafio099.py
@@ -1,4 +1,14 @@
 
 #*Faça um programa que tenha uma FUNÇÃO chamada MAIOR(), que receba vários PARÂMETROS com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles e o MAIOR.
 
+from random import randint
 
+def maior():
+    print('-='*30)
+    print('Analisando os valores passados...')
+    numero = randint(0,15)
+    for x in range(numero):
+        print(f'{x}', end=' ')
+    print('\n','-='*30)
+
+maior()
