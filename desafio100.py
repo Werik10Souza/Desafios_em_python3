76db7ef65bff9f48f87df6843e1231bdd9f19de1
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-21 17:04:17 -0300

    update

---
+++ desafio100.py
@@ -0,0 +1,2 @@
+
+#*Faça um programa que tenha uma LISTA chamada NÚMEROS e duas FUNÇÕES chamadas SORTEIA() e SOMAPAR(). A primeira vai sortear 5 números e vai colocá-los dentro da LISTA e a segunda FUNÇÃO vai mostrar a SOMA entre todos os valores PARES sorteados pela função ANTERIOR.
