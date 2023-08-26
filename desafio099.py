f96327cac43d20b92a6a5f54568556c2a61b9fba
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-26 15:12:23 -0300

    update

--- desafio099.py
+++ desafio099.py
@@ -1,18 +1,29 @@
 
 #*Faça um programa que tenha uma FUNÇÃO chamada MAIOR(), que receba vários PARÂMETROS com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles e o MAIOR.
 
-from random import randint
+from time import sleep
 
-lista = []
+def maior(* numeros):
+    contador = 0
+    maior = 0
+    print('-=' * 30)
+    print('Analisando os valores passados... ')
+    for valor in numeros:
+        print(f'{valor} ', end='', flush=True)
+        sleep(0.3)
+        if contador == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        contador = contador + 1
+    print(f'Foram informados {contador} valores ao todo.')
+    print(f'O maior valor informado foi {maior}')
 
-def maior():
-    print('-='*30)
-    print('Analisando os valores passados...')
-    numero = randint(0,10)
-    for x in range(numero):
-        lista.append(x)
-    print(f'{lista} foram informados {len(lista)} ao todo')
-    print(f'O maior valor informado foi {max(lista)}')
-    print('-='*30)
 
+#?PROGRAMA PRINCIPAL
+maior(2, 9, 4, 5, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
 maior()
