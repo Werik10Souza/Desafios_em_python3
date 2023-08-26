3fef240e73f4d19d82313fb809ff36992920de6d
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-26 15:57:48 -0300

    update

--- desafio100.py
+++ desafio100.py
@@ -2,27 +2,23 @@
 #*Faça um programa que tenha uma LISTA chamada NÚMEROS e duas FUNÇÕES chamadas SORTEIA() e SOMAPAR(). A primeira vai sortear 5 números e vai colocá-los dentro da LISTA e a segunda FUNÇÃO vai mostrar a SOMA entre todos os valores PARES sorteados pela função ANTERIOR.
 
 from random import randint
+from time import sleep
 
-soma = 0
-par = 0
+def sorteia(lista):
+    for contador in range(0,5):
+        num = randint(1, 10)
+        lista.append(num)
+        print(f'{num} ', end='', flush=True)
+        sleep(0.3)
+    print('PRONTO!')
 
-numeros = []
+def somapar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma = soma + valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
 
-def sorteia():
-    print('-='*30)
-    valores = (randint(0,4), randint(0,4), randint(0,4), randint(0,4), randint(0,4))
-    for v in valores:
-        numeros.append(v)
-    print(numeros)
-
-sorteia()
-
-def somapar():
-    par = numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4]
-    if par % 2 == 0:
-        par = sum(numeros)
-
-    print(f'\nSOMA DOS NÚMEROS PARES: {par}')
-    print('-='*30)
-
-somapar()
+numeros = list()
+sorteia(numeros)
+somapar(numeros)
