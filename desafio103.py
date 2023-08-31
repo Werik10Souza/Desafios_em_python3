f4c780e17fd700c2775a541bb8b7dbefbec521cb
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-31 12:18:36 -0300

    update

---
+++ desafio103.py
@@ -0,0 +1,4 @@
+
+#*Faça um programa que tenha uma FUNÇÃO chamada FICHA(), que receba dois PARÂMETROS OPCIONAIS: o NOME de um jogador e quantos GOLS ele marcou.
+
+#*O programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não tenha informado corretomente.
