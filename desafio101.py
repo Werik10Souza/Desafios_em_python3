cd19d56c54c5a9a1ab635dac4891a6117e9d35e8
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-31 16:30:52 -0300

    update

--- desafio101.py
+++ desafio101.py
@@ -1,2 +1,6 @@
 
 #*Crie um programa que tenha uma FUNÇÃO chamada VOTO() que vai receber como PARÂMETRO o ANO DE NASCIMENTO de uma pessoa retornanda um valor LITERAL indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIA nas eleições.
+
+from datetime import date
+
+
