435fec5333f3a37e177812ca7c5ae1dfb2539e1e
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-31 12:35:50 -0300

    update

---
+++ desafio105.py
@@ -0,0 +1,10 @@
+
+#*Faça um programa que tenha uma FUNÇÃO NOTAS() que pode receber várias notas de alunos e vai retornar um DICIONÁRIO com os sequintes informações:
+
+#* - QUANTIDADE DE NOTAS.
+#* - A MAIOR NOTA.
+#* - A MENOR NOTA.
+#* - A MÉDIA DA TURMA
+#* - A SITUAÇÃO(OPCIONAL)
+
+#*Adicione também os "docstrings" de função.
