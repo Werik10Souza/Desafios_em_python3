24e30838bd2b900d95364d842cfc43736e810e14
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-31 12:10:36 -0300

    update

---
+++ desafio102.py
@@ -0,0 +1,2 @@
+
+#*Crie um programa tenha uma FUNÇÃO FATORIAL() que receba dois parâmetro: o primeiro que indique o número a calcular e o outro chamado SHOW, que será um valor LÓGICO(OPCIONAL) indicando se será mostrado ou não na tela processo de cálculo do fatorial.
