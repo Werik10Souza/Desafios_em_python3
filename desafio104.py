98b5c953740764d12f33fd96abb9323644a0030f
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-08-31 12:29:56 -0300

    update

---
+++ desafio104.py
@@ -0,0 +1,5 @@
+
+#*Crie um programa que tenha a FUNÇÃO leiaInt(), que vai funcionar de forma semelhente á função input() da python, só que fazendo VALIDAÇÃO para aceitar apenas um valor número.
+
+#?EX:
+#? n = leiaInt('Digite um n: ')
