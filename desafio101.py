01da169f6a40ef45c780fe7d3888fe227fa0cd2d
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-01 15:06:10 -0300

    update

--- desafio101.py
+++ desafio101.py
@@ -1,4 +1,17 @@
 
 #*Crie um programa que tenha uma FUNÇÃO chamada VOTO() que vai receber como PARÂMETRO o ANO DE NASCIMENTO de uma pessoa retornanda um valor LITERAL indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIA nas eleições.
 
+def voto(ano):
+    from datetime import date
+    data = date.today().year
+    idade = data - ano
+    if idade <= 17:
+        return(f'Com {idade} anos: VOTO NEGADO')
+    else:
+        if idade >= 18 and idade <= 64:
+            return(f'Com {idade} anos: VOTO OBRIGATÓRIO')
+        else:
+            return(f'Com {idade} anos: VOTO OPCIONAL')
 
+ano_nasc = int(input('Em qual ano você nasceu?: '))
+print(voto(ano_nasc))
