21b4485f7ce30660aef96deb54bf26a8c5a955b4
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-03 14:59:17 -0300

    update

--- desafio102.py
+++ desafio102.py
@@ -1,7 +1,7 @@
 
 #*Crie um programa tenha uma FUNÇÃO FATORIAL() que receba dois parâmetro: o primeiro que indique o número a calcular e o outro chamado SHOW, que será um valor LÓGICO(OPCIONAL) indicando se será mostrado ou não na tela processo de cálculo do fatorial.
 
-def fatorial(num, show=False):
+def fatorial(num, show=False):#SHOW OPCIONAL
 
     """
     fatorial(num, show = False)
@@ -13,20 +13,20 @@ def fatorial(num, show=False):
 
     print('-'*40)
 
-    contador = num
+    contador = num #O contador vai receber o número que for inserido no parâmentro
     f = 1
 
     while contador > 0:
-        if show:
+        if show: #Essa linha determinar se vai aparecer ou não o cálculo
             print(f'{contador}', end=' ')
             if contador > 1:
                 print(' X ', end=' ')
             else:
                  print(' = ', end=' ')
 
-        f = f * contador
-        contador = contador - 1
-    return(f'{f}')
+        f = f * contador # f -> FATORIAL vezes(X) o contador
+        contador = contador - 1 #Vai conta do MAIOR para o MENOR número
+    return(f'{f}') #Valor da multiplicação feita na linha 27
 
 
 #?PROGRAMA PRINCIPAL:
