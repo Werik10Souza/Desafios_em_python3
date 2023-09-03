401c2b79fd7289b6f1ba51e53c6ffcf992e450cd
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-03 14:10:15 -0300

    update

--- desafio102.py
+++ desafio102.py
@@ -2,7 +2,14 @@
 #*Crie um programa tenha uma FUNÇÃO FATORIAL() que receba dois parâmetro: o primeiro que indique o número a calcular e o outro chamado SHOW, que será um valor LÓGICO(OPCIONAL) indicando se será mostrado ou não na tela processo de cálculo do fatorial.
 
 def fatorial(num, show=0):
-    print('-='*20)
+
+    """
+    fatorial(num, show = False)
+    -> Calcula o fatorial de um número.
+    :param num: O número a ser calculodo.
+    :param show: (Opcional) mostrar ou não a contra.
+    :return: O valor do fatorial de número num.
+    """
 
     contador = num
     f = 1
@@ -16,5 +23,7 @@ def fatorial(num, show=0):
         contador = contador - 1
     return(f'{f}')
 
+
 #?PROGRAMA PRINCIPAL:
-print(fatorial(num = int(input('Digite um número: '))))
+#print(fatorial(num = 5, show=True))
+help(fatorial)
