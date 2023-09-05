bb09506e8a4adf3d58bfe36a82c25b44155e2be6
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-05 15:09:08 -0300

    update

--- desafio103.py
+++ desafio103.py
@@ -3,3 +3,27 @@
 
 #*O programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não tenha informado corretomente.
 
+def ficha(n = 0, g = 0):
+    print('-'*40)
+    
+    if ficha(n,g):
+        return(f'O jogador {n} marcou {g} gols')
+    elif ficha(n):
+        return(f'O jogador {n} marcou {g} gols')
+    elif ficha(g):
+        if n == 0:
+            n = str(' <DESCONHECIDO> ')
+        return(f'O jogador {n} marcou {g} gols')
+    elif ficha():
+        if n == 0:
+            n = str(' <DESCONHECIDO> ')
+        return(f'O jogador {n} marcou {g} gols')
+
+#?PROGRMA PRINCIPAL
+nome = str(input('Digite o nome do jogador: '))
+gols = int(input(f'Quantos gols o jogador {nome} marcou?: '))
+
+ficha(nome, gols)
+#ficha(nome)
+#ficha(gols)
+#ficha()
