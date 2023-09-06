7548bdf5e7f9c9ea50e292b3f8a02a79d950b103
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-06 12:53:47 -0300

    update

--- desafio103.py
+++ desafio103.py
@@ -3,23 +3,20 @@
 
 #*O programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não tenha informado corretamente.
 
-def ficha(n = '', g=0):
-    #!ESCOPO LOCAL
-    g = 0
+def ficha(n = '', g = 0):
+    print('-'*50)
+    g = 0 #?ESCOPO GLOBAL 
     if nome == '':
-        return f'O jogador <desconhecido> fez {gols} gol(s) no campeonato'
+        return(f'O jogador <desconhecido> marcou {gols} gol(s) no campeonato')
     elif gols == '':
-        return f'O jogador {n} fez {g} gol(s) no campeonato'
+        return(f'O jogador {n} marcou {g} gol(s) no campeonato')
     else:
-        return f'O jogador {n} fez {gols} gol(s) no campeonato'
+        return(f'O jogador {n} marcou {gols} gol(s) no campeonato')
 
-#programa principal:
+
+#?PROGRAMA PRINCIPAL
 #!ESCOPO GLOBAL
-nome = str(input('Nome do jogador: '))
-gols = str(input('Números de gols: '))
+nome = str(input('Digite o nome do jogador: ')).strip()
+gols = str(input(f'Quantos gols o jogador {nome} marcou?: '))
 
-#print(ficha(nome, gols))
-#print(ficha(nome))
-#print(ficha(gols))
-#print(ficha())
-
+print(ficha(nome))
