d45ba208721eb370dd8615c1c58e266c7cfa0900
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-06 10:43:07 -0300

    update

--- desafio103.py
+++ desafio103.py
@@ -1,9 +1,10 @@
 
 #*Faça um programa que tenha uma FUNÇÃO chamada FICHA(), que receba dois PARÂMETROS OPCIONAIS: o NOME de um jogador e quantos GOLS ele marcou.
 
-#*O programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não tenha informado corretomente.
+#*O programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não tenha informado corretamente.
 
 def ficha(n = '', g=0):
+    #!ESCOPO LOCAL
     g = 0
     if nome == '':
         return f'O jogador <desconhecido> fez {gols} gol(s) no campeonato'
@@ -13,11 +14,12 @@ def ficha(n = '', g=0):
         return f'O jogador {n} fez {gols} gol(s) no campeonato'
 
 #programa principal:
+#!ESCOPO GLOBAL
 nome = str(input('Nome do jogador: '))
 gols = str(input('Números de gols: '))
 
 #print(ficha(nome, gols))
 #print(ficha(nome))
 #print(ficha(gols))
-print(ficha())
+#print(ficha())
 
