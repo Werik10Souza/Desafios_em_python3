a10b988bc8839c9c341c0d25c8163213aeed6ce2
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-08 14:21:22 -0300

    update

--- desafio104.py
+++ desafio104.py
@@ -3,3 +3,21 @@
 
 #?EX:
 #? n = leiaInt('Digite um n: ')
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033mERRO!Digite um número inteiro válido\033[m')
+        if ok:
+            break
+    return valor
+
+#*PROGRAMA PRINCIPAL
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
