2aec52a3674b091a003db9d6925a8aed5819fbcf
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-10 16:34:09 -0300

    update

--- desafio105.py
+++ desafio105.py
@@ -8,3 +8,34 @@
 #* - A SITUAÇÃO(OPCIONAL)
 
 #*Adicione também os "docstrings" de função.
+
+def nota():
+    lista = []
+    contador = 0
+    soma = 0
+    pergunta = 'N'
+    while True:
+        nota = float(input('Digite a nota do aluno: '))
+        soma = soma + nota
+        lista.append(nota)
+        alunos = {'Total': lista, 'Maior': lista, 'Menor': lista, 'Média': lista}
+
+        contador = contador + 1
+
+        media = soma/contador
+
+        alunos['Total'] = contador
+        alunos['Maior'] = max(lista)
+        alunos['Menor'] = min(lista)
+        alunos['Média'] = f'{media:.2f}'
+
+        mostra = f'TOTAL: {alunos["Total"]}, MAIOR: {alunos["Maior"]}, MENOR: {alunos["Menor"]}, MÉDIA: {alunos["Média"]}'
+        
+        pergunta = str(input('Quer continuar[S/N]?: ').upper().strip() [0])
+        if pergunta == 'N':
+            break
+        
+    print(mostra)
+
+#?PROGRAMA PRINNCIPAL        
+print(nota())
