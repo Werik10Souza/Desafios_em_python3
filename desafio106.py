4c39360e9328fe0c5b6ab5ad1a450addc38dcfa3
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-11 12:39:38 -0300

    update

--- desafio106.py
+++ desafio106.py
@@ -2,3 +2,16 @@
 #*Faça um MINI-SISTEMA que utilize o 'Interactive Help' do python. O usuário digitar a palavra  o 'comando' e o MANUAL vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se ENCERRAR 
 
 #*OBS: USE CORES
+
+def mini_sistema(p= ' '):
+    global interativo
+    mensagem = 'SISTEMA DE AJUDAR PYTHON'
+    while True:
+        print('~'*len(mensagem))
+        print('SISTEMA DE AJUDAR PYTHON')
+        print('~'*len(mensagem))
+        interativo = str(input('\nFunção ou Biblioteca >  ').strip())
+        help(interativo)
+
+#?PROGRAMA PRINCIPAL
+mini_sistema()
