202bf8fd331b9781837db344f0ff8ac6563120b3
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-11 13:33:29 -0300

    update

--- desafio106.py
+++ desafio106.py
@@ -5,13 +5,22 @@
 
 def mini_sistema(mensagem):
     global interativo
-    msn = 'SISTEMA DE AJUDAR PYTHON'
+    msn01 = 'SISTEMA DE AJUDAR PYTHON'
+    msn02 = 'ATÉ LOGO!'
+    comando = 'fim'
     while True:
-        print('~'*len(msn))
+        print('~'*len(msn01))
         print('SISTEMA DE AJUDAR PYTHON')
-        print('~'*len(msn))
+        print('~'*len(msn01))
         interativo = str(input(mensagem).strip())
-        help(interativo)
+
+        if interativo == comando:
+            print('~'*len(msn02))
+            print('ATÉ LOGO!!!')
+            print('~'*len(msn02))
+            break
+        else:
+            help(interativo)
 
 #?PROGRAMA PRINCIPAL
 mini_sistema('\nFunção ou Biblioteca >  ')
