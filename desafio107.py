b85aded7ecb753d53d22d9c1b99a8915bf8d7028
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-09-30 15:19:31 -0300

    update

--- desafio107.py
+++ desafio107.py
@@ -2,3 +2,12 @@
 #*Crie um módulo chamado "moeda.py" que tenha as funções incorporadas 'aumentar(), diminuir(), dobro(), e metade().'
 
 #*Faça também um programa que 'importe' esse módulo e use algumas dessas funções.
+
+import moeda
+
+valor = float(input('Digite o valor: '))
+
+print(f'O valor aumentando -> {moeda.aumentar(valor)}')
+print(f'O valor diminuindo ->  {moeda.diminuir(valor)}')
+print(f'O valor dobrando -> {moeda.dobro(valor)}')
+print(f'O valor dividindo -> {moeda.metade(valor)}')
---
+++ moeda.py
@@ -0,0 +1,15 @@
+def aumentar(v=0):
+    r = v + 1
+    return r
+
+def diminuir(v=0):
+    r = v - 1
+    return r
+
+def dobro(v=0):
+    r = v * 2
+    return r
+
+def metade(v=0):
+    r = v / 2
+    return r
