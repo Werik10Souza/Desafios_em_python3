13573a44d7a1d89ac737f57f385a2e17d4f806bd
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-01 15:04:49 -0300

    update

--- desafio110.py
+++ desafio110.py
@@ -1,4 +1,7 @@
 
 #*Adicione ao módulo 'moeda.py' criado nos desafios anteriores, uma função chamanda "resumo()", que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
 
+import ex110
 
+valor = float(input('Digite o preço: R$'))
+ex110.resumo(valor, 80, 35)
