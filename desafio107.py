e34388553563efac038999a4bffceddbf805ef47
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-01 01:33:15 -0300

    update

--- desafio107.py
+++ desafio107.py
@@ -7,7 +7,7 @@ import moeda
 
 valor = float(input('Digite o preço: '))
 
-print(f'Aumentando em 10% é {moeda.aumentar(valor, 10)}')
-print(f'Diminuindo em 10%  é {moeda.diminuir(valor, 10)}')
-print(f'O dobro de {valor} é {moeda.dobro(valor)}')
-print(f'A metade de {valor} é {moeda.metade(valor)}')
+print(f'Aumentando em 10% é \033[0;32m{moeda.aumentar(valor, 10)}\033[m')
+print(f'Diminuindo em 10%  é \033[0;32m{moeda.diminuir(valor, 10)}\033[m')
+print(f'O dobro de \033[0;35m{valor}\033[m é \033[0;32m{moeda.dobro(valor)}\033[m')
+print(f'A metade de \033[0;35m{valor}\033[m é \033[0;32m{moeda.metade(valor)}\033[m')
--- desafio109.py
+++ desafio109.py
@@ -1,2 +1,4 @@
 
 #*Modifique as funções que foram criadas no 'desafio107' para que elas aceitem um "um parâmetro" a mais, informando se o valor retornado por elas vai ser ou não formatado pela função 'moeda()', desenvolvida no 'desafio 108'.
+
+valor = float(input('Digite o preço: R$'))
