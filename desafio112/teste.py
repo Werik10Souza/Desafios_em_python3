1819f07058b1ba35a980f10eedfbd63fa3d6380d
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-11 14:09:39 -0300

    update

--- desafio112/teste.py
+++ desafio112/teste.py
@@ -1,7 +1,7 @@
 
 #*Dentro do pacote 'utilidadesCeV' que criamos no 'desafio 111, temos um "módulo" chamado "leiaDinheiro()" que seja capaz de funcionar como a função "input()", mas com uma 'validção de dados' para aceitar apenas valores que sejam "monetários".
 
-from desafio112.utilidadescev import moeda
+from utilidadescev import moeda
 
 valor = float(input('Digite o preço: R$'))
 moeda.resumo(valor, 20, 12)
