41269a9f8e423b4a3d8f3da9d9981f0b14ac03f3
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-11 14:42:32 -0300

    update

--- desafio112/utilidadescev/dados/__init__.py
+++ desafio112/utilidadescev/dados/__init__.py
@@ -1,7 +1,7 @@
 def leiaDinheiro(msg):
     valido = False
     while not valido:
-        entrada = str(input(msg))
+        entrada = str(input(msg)).replace(',', '.')
         if entrada.isalpha():
             print(f'\033[0;31mERRO: \"{entrada}\" é um preço inválido!\033[m')
         else:
