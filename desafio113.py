26c42a8da03e450cd4f29c8a50ee271217ddbcd6
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-23 15:42:17 -0300

    update

--- desafio113.py
+++ desafio113.py
@@ -1,4 +1,21 @@
 
 #*Reescreva a função leiaInt() que fizemos no  desafio104, incluindo agora a possibilidade da digitação de um número de tipo inválido. Aproveite e crie tmbém uma função leiaFloat() com a mesma funcionalidade.
 
+def leiaInt(mensagem):
+    situacao =  False
+    valor = 0
+    while True:
+        numero = str(input(mensagem))
+        if numero.isnumeric():
+            valor = int(numero)
+            situacao = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if situacao:
+            break
+    return valor
 
+
+#*Programa principal:
+numero = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {numero}')
