756ad879fa5349d7cd5fd7669b4e8f821da3d09e
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-25 00:01:42 -0300

    update

--- desafio113.py
+++ desafio113.py
@@ -9,9 +9,9 @@ def leiaInt(mensagem): #Função leiaInt: Essa função permite ao usuário digi
 def leiaFloat(mensagem): #permite ao usuário digitar um número de ponto flutuante (número real).
     while True:  #Laço while True: Iniciamos um loop infinito com while True, o que significa que ele continuará pedindo a entrada até que o usuário forneça um valor válido.
         try: #tenta 
-            valor = float(input(mensagem)) #tenta converter a entrada do usuário em um número inteiro usando.
-            return valor #Se o usuário fornecer um valor válido que pode ser convertido em um número inteiro, o código sairá do loop e retornará o valor.
-        except ValueError: #Se o usuário fornecer um valor que não pode ser convertido em um número flotuante.
+            valor = float(input(mensagem)) #tenta converter a entrada do usuário em um número flutuante usando.
+            return valor #Se o usuário fornecer um valor válido que pode ser convertido em um número flutuante, o código sairá do loop e retornará o valor.
+        except ValueError: #Se o usuário fornecer um valor que não pode ser convertido em um número flutuante.
             print('\033[0;31mERRO: Digite um número inteiro válido.\033[m')  #informar ao usuário que a entrada foi inválida.
 
 #*Programa principal:
