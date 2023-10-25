fbe95783ed7798eafe5d94652d4bd0ea14272c13
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-25 20:18:24 -0300

    update

--- desafio115.py
+++ desafio115.py
@@ -2,3 +2,17 @@
 #*Crie um pequeno sistema modularizadi que permita cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
 
 #*O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todos as pessoas cadastradas.
+
+from menu import leiaOpcoes
+
+#*Programa Principal
+while True:
+    escolha = leiaOpcoes('\033[0;35mSua Opção:\033[m ')
+    if escolha == 1:
+        print('\033[0;32mLista dos nomes e idade...\033[m')
+    elif escolha == 2:
+        print('\033[0;32mCadastrar Pessoas Novas Aqui...\033[m')
+    elif escolha == 3:
+        break
+print('\033[0;38mFim do programa...\033[m')
+    
---
+++ menu.py
@@ -0,0 +1,14 @@
+def leiaOpcoes(opcoes):
+    print('-'*20)
+    print('    MENU PRINCIPAL      ')
+    print('-'*20)
+    print('1- \033[0;36mVer Pessoas Cadastradas\033[m')
+    print('2- \033[0;36mCadastrar Nova Pessoa\033[m')
+    print('3- \033[0;36mSair Do Sistema\033[m')
+    while True:
+        try:
+            escolha = int(input('Sua Opção: '))
+            if escolha in [1, 2 , 3]:
+                return escolha
+        except ValueError:
+            print('\033[0;31mErro! Escolha a opção 1, 2 ou 3.\033[m')
