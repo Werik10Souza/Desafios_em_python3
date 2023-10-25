f4499d5af66b376d5aeb23894899a79282100049
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-25 17:06:07 -0300

    update

--- desafio114.py
+++ desafio114.py
@@ -11,3 +11,4 @@ except urllib.error.URLError:
     print('\033[0;31mO site pudim não está acessível no momento.\033[m')
 else:
     print('\033[0;32mConsegui acessar o site pudim com sucesso!\033[0;32m')
+    print(site.read())
