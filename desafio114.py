6827da240ce16a799e988bdfba77869c370ddb05
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-25 16:58:15 -0300

    update

--- desafio114.py
+++ desafio114.py
@@ -1,2 +1,13 @@
 
+
 #*Crie um código em python que teste se o site pudim está acessível pelo computador usado.
+
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://pudim.com.br')
+except urllib.error.URLError:
+    print('\033[0;31mO site pudim não está acessível no momento.\033[m')
+else:
+    print('\033[0;32mConsegui acessar o site pudim com sucesso!\033[0;32m')
