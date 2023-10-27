63b55d74af098679959bab80dc1f344302177e24
Werik Souza <106021978+Werik10Souza@users.noreply.github.com>
2023-10-26 21:10:48 -0300

    update

--- desafio115.py
+++ desafio115.py
@@ -1,5 +1,5 @@
 
-#*Crie um pequeno sistema modularizadi que permita cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
+#*Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
 
 #*O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todos as pessoas cadastradas.
 
